4dbd4695ee15902f23a67606250505887e521666
sabareesh-r23 <sabareeshr23@gmail.com>
2021-06-15 12:51:18 +0530

    trial commit

--- utils/webUtils.js
+++ utils/webUtils.js
@@ -9,13 +9,66 @@ module.exports = {
 
   async clearAndType(elementSelector,typeText){
     await I.waitForVisible(elementSelector,process.env.WAIT_LOW)
-    await I.clearField(elementSelector)
     await I.fillField(elementSelector,typeText)
   },
 
   async typeText(elementSelector,typeText){
     await I.waitForVisible(elementSelector,process.env.WAIT_LOW)
-    await I.fillField(elementSelector,typeText)
-  }
+    await I.appendField(elementSelector,typeText)
+  },
+
+  async doubleClickElement(elementSelector){
+    await I.waitForVisible(elementSelector,process.env.WAIT_LOW)
+    await I.doubleClick(elementSelector)
+  },
+
+  async selectRadioButton(elementSelector){
+    await I.waitForClickable(elementSelector,process.env.WAIT_LOW)
+    await I.selectRadioButton(elementSelector)
+  },
+  
+  async getTotalTableRow(){
+
+  },
+
+  async getTotalTableColumn(){
+
+  },
+
+  async getCellDataFromTableUsingRowAndColumn(){
+
+  },
+
+  async getCellDataFromTableUsingRowAndColumnLabel(){
+
+  },
+
+  async getTextValueFromElement(element){
+    await I.waitForVisible(element,process.env.WAIT_LOW)
+    const text = await I.grabTextFrom(element)
+    return text
+  },
+
+  async getAttributeValueFromElement(element){
+    await I.waitForVisible(element,process.env.WAIT_LOW)
+    const value = await I.grabAttributeFrom(element)
+    return value
+  },
+
+  //File path relative to current codecept
+  async attachFile(element,filepath){
+    await I.waitForVisible(element,process.env.WAIT_LOW)
+    await I.attachFile(element,filepath)
+  },
+
+  async getPageTitle(){
+    const title = await I.grabTitle()
+    return title
+  },
+
+  async moveCursorTo(element){
+    await I.waitForElement(element,process.env.WAIT_LOW)
+    await I.moveCursorTo(element)
+  },
   
 }
